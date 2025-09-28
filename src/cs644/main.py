a0f5554be3121acd3e278a6525692acfcf5cd65e
Rachel K <rachel.b.king@gmail.com>
2025-09-28 22:50:43 +0000

    Initial commit + basic run/count functions

---
+++ src/cs644/main.py
@@ -0,0 +1,51 @@
+import argparse
+import os
+
+LOG_FILE_PATH = "/home/rachel/cs644/server.log"
+
+def run():
+    print("running")
+    # open file, create if not exist
+    # TODO to think about: locking
+    fd = os.open(LOG_FILE_PATH, os.O_WRONLY | os.O_CREAT | os.O_APPEND)
+    # append line to log
+    log_line = 'this is a log line'.encode()
+    os.write(fd, log_line)
+    # close file
+    os.close(fd)
+
+def count():
+    print("counting")
+    line_count = 0
+    try:
+        # read log file and print count of lines
+        fd = os.open(LOG_FILE_PATH, os.O_RDONLY)
+        # empty file has empty bytestring
+        # file with one line has no newlines but non-empty bytestring
+        output = os.read(fd, 20).decode()
+        # in a for loop, read file. keep counter of number of lines
+        while output != '':
+            line_count += len(output.splitlines())
+            # don't overcount the second part of a line that has already been partially read
+            output = os.read(fd, 20).decode().split('\n', 1)[-1]
+        os.close(fd)
+    # rescue FileNotFoundError and return 0
+    except FileNotFoundError:
+        pass
+    finally:
+        print(f"Line count: {line_count}")
+
+def main():
+    # argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--run', action='store_true')
+    parser.add_argument('-c', '--count', action='store_true')
+    args = parser.parse_args()
+
+    if args.count:
+        count()
+    elif args.run:
+        run()
+
+if __name__ == "__main__":
+    main()
